acdca109ee2885ad91a9e46dae738ff1cbf6b1a0
Gabriel Oliveira <115432420+Goliver10@users.noreply.github.com>
2022-11-06 22:54:48 -0300

    Add files via upload
    Projeto DIO 
    Criando um Deploy de uma Aplicação com scripts em .bat e .sh para o provisionamento.

---
+++ ProjetoDio/script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "Criando as imagens..."
+
+docker build -t goliver10/projeto-backend:1.0 backend/.
+docker build -t goliver10/projeto-database:1.0 database/.
+
+echo "Fazendo o push das imagens..."
+
+docker push goliver10/projeto-backend:1.0
+docker push goliver10/projeto-database:1.0
+
+echo "Criando serviços no cluster kubernetes..."
+
+kubectl apply -f ./services.yml
+
+echo "Criando os deployments..."
+
+kubectl apply -f ./deployment.yml
